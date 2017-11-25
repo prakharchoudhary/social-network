5811754493e2229bca661d454ced1530e8685550
prakharchoudhary <prakhar2397@gmail.com>
2017-11-25 22:02:10 +0530

    fix import errors

--- app/__init__.py
+++ app/__init__.py
@@ -12,17 +12,17 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+  app = Flask(__name__)
+  app.config.from_object(config[config_name])
+  config[config_name].init_app(app)
 
-    bootstrap.init_app(app)
-    mail.init_app(app)
-    moment.init_app(app)
-    db.init_app(app)
+  bootstrap.init_app(app)
+  mail.init_app(app)
+  moment.init_app(app)
+  db.init_app(app)
 
-    # attach routes and custom error pages here
-    from main import main as main_blueprint
-	app.register_blueprint(main_blueprint)
+  # attach routes and custom error pages here
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
 
-    return app
+  return app
