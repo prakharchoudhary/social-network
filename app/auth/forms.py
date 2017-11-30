1d9583357fd2af182c783063f15727eb40598cf2
prakharchoudhary <prakhar2397@gmail.com>
2017-11-30 07:06:20 +0530

    add login logout

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm as Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, Length
 
 
 class LoginForm(Form):
